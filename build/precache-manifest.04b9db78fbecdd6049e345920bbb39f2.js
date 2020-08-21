ac44daa01c3f497ae37119e9134ea7da51de78a7
Dishant Kumar Kushwaha <dishantkushwaha71@gmail.com>
2020-08-21 20:18:03 +0530

    Initial

---
+++ build/precache-manifest.04b9db78fbecdd6049e345920bbb39f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aa2e6975cfc2a2e2f0486641b72c9bc",
+    "url": "/my-portfolio-app/index.html"
+  },
+  {
+    "revision": "994f5bd4c3a80ae819d1",
+    "url": "/my-portfolio-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f4d64557f7246b131ae7",
+    "url": "/my-portfolio-app/static/js/2.6385edd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-portfolio-app/static/js/2.6385edd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "994f5bd4c3a80ae819d1",
+    "url": "/my-portfolio-app/static/js/main.ca0649ff.chunk.js"
+  },
+  {
+    "revision": "2e01c44ac1564f490570",
+    "url": "/my-portfolio-app/static/js/runtime-main.b0f29e76.js"
+  }
+]);
