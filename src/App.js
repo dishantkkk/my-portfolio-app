620e75db208d5ab8914c8da2970e941163dca373
Dishant Kumar Kushwaha <dishantkushwaha71@gmail.com>
2020-08-21 20:06:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,19 +11,20 @@ import './App.css';
 
 
 
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header"/>
-      <About resumeData={resumeData}/>
-      <Resume resumeData={resumeData}/>
-      <Portfolio resumeData={resumeData}/>
-      <Testimonials resumeData={resumeData}/>
-      <ContactUs resumeData={resumeData}/>
-      <Footer resumeData={resumeData}/>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header resumeData={resumeData}/>
+        <About resumeData={resumeData}/>
+        <Resume resumeData={resumeData}/>
+        <Portfolio resumeData={resumeData}/>
+        <Testimonials resumeData={resumeData}/>
+        <ContactUs resumeData={resumeData}/>
+        <Footer resumeData={resumeData}/>
+      </div>
+    );
+  }
 }
 
 export default App;
